f90b0dbf5e5ca9e52e5ea2de522c08a198becc86
Patrick <pvilchez794@gmail.com>
2020-08-28 01:19:37 -0500

    enemy ship movement added

--- public/js/index.js
+++ public/js/index.js
@@ -1,57 +1,69 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
-// 143 - 130
 let positionX = (canvas.width / 2) - 7;
 let positionY = canvas.height - 15;
 
-// let limitX = canvas.width;
-// let limitY =;
+let enemyPosX = 45;
+let enemyPosY = 35;
  
-let speedX = 5;
-let speedY = -5;
+let speedX = 6;
+let speedY = -6;
 
-function drawShip() {
-    ctx.beginPath();
-    ctx.fillStyle = 'rgb(300, 0, 0)';
-    ctx.fillRect(positionX, positionY, 14, 5);
-    ctx.closePath();
-}
+let enemySpeedX = 0.3;
+let enemySpeedY = -0.3;
 
 window.addEventListener('keydown', (event) => {
-    if (event.key === 'w') {
-        // moveUp();
-        console.log('w');
-    } else if (event.key === 'd') {
+    if (event.key === 'd') {
         moveRight();
         console.log('d');
-    } else if (event.key === 's') {
-        // moveDown();
-        console.log('s');
     } else if (event.key === 'a') {
         moveLeft();
         console.log('a');
     }
 });
 
-function moveLeft() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+function drawShip() {
+    ctx.beginPath();
+    ctx.fillStyle = '#0095DD';
+    ctx.fillRect(positionX, positionY, 14, 5);
+    ctx.closePath();
+}
 
+function drawEnemyShips() {
+    ctx.beginPath();
+    ctx.fillStyle = '#0095DD';
+    ctx.fillRect(enemyPosX, enemyPosY, 25, 5);
+    ctx.closePath();
+}
+
+function moveLeft() {
     if (positionX > 15) {
         positionX -= speedX;
     }
-    
-    drawShip();
 }
 
 function moveRight() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     if (positionX < (canvas.width - 29)) {
         positionX += speedX;
     }
-    
-    drawShip();
 }
 
-drawShip();
+function drawAll() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawShip();
+    drawEnemyShips();
+
+    if (enemyPosX > (canvas.width - 40) || enemyPosX < 15) {
+        enemySpeedX = -enemySpeedX;
+    }
+    
+    enemyPosX += enemySpeedX;
+
+    console.log(canvas.width);
+    console.log(enemyPosX);
+}
+
+
+
+setInterval(drawAll, 10);
